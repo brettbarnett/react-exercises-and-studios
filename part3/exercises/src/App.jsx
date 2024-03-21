bcabbefcdbf1c87d723b1ebda740571ffaa4fb45
Brett Barnett <brett_barnett@sbcglobal.net>
2024-03-21 13:21:06 -0500

    Completed React Part 3 Exercise

--- part3/exercises/src/App.jsx
+++ part3/exercises/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import HobbyIntroduction from './components/Introduction';
-import MyProjects from './components/Projects';
+import { useState } from "react";
+import "./App.css";
+import HobbyIntroduction from "./components/Introduction.jsx";
+import MyProjects from "./components/Projects.jsx";
 
 function App() {
-
   return (
-    <>
-      <div className="App">
-	  <h1>My Hobby: Needlepoint</h1>
-	  <HobbyIntroduction />
-	  <MyProjects />
-      </div>
-    </>
-  )
+    <div className="App">
+      <h1>My Hobby: Learning To Code</h1>
+      <HobbyIntroduction />
+      <MyProjects />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,12 +1,14 @@
-export default function HobbyIntroduction () {
-   return(
-      <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
-         <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
-         </ol>
-      </div>
-   );
+function HobbyIntroduction() {
+  return (
+    <div>
+      <h2>3 Fun Facts About Coding</h2>
+      <ol>
+        <li>There are lots of languages and libraries/frameworks</li>
+        <li>There are compiled languages and interpreted languages</li>
+        <li>All modern browsers support CSS and JavaScript</li>
+      </ol>
+    </div>
+  );
 }
+
+export default HobbyIntroduction;
